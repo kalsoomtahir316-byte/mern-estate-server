013fd78a909dfc159b52111113d30c0c146fcf70
Kalsoom Tahir <kalsoomtahir316@gmail.com>
2025-11-11 20:08:09 -0800

    fix: ensure /api routes handled correctly for Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
 // api/index.js  (repo root par 'api' folder ke andar)
-import app from "../src/index.js";   // path bilkul yahi rahe
-export default app;                  // Express app ko Vercel ko export karo
+import app from "../src/index.js";  
+export default app;                  
